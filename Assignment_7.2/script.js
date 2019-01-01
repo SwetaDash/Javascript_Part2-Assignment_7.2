31979bbb4f9cf0816cb18dc0264499f47af57f6b
SwetaDash <43117650+SwetaDash@users.noreply.github.com>
2019-01-01 16:43:05 +0530

    Add files via upload

---
+++ Assignment_7.2/script.js
@@ -0,0 +1,12 @@
+var i;
+console.log("Using For loop\n");
+for (i = 1; i <= 10; i++) {
+    console.log(i * 5);
+}
+
+console.log("Using While loop\n");
+i = 1;
+while (i <= 10) {
+    console.log(i * 5);
+    i++;
+}
